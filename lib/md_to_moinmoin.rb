58ffeee691ceb33525f2174565b9c6df02bcb47b
catatsuy <catatsuy@catatsuy.org>
2013-10-09 15:42:39 +0900

    thx https://gist.github.com/Lysander/2689105

--- lib/md_to_moinmoin.rb
+++ lib/md_to_moinmoin.rb
@@ -1,5 +1,43 @@
 require "md_to_moinmoin/version"
+require 'redcarpet'
 
 module MdToMoinmoin
-  # Your code goes here...
+  class Moinmoin < Redcarpet::Render::Base
+    def header(text, level)
+      prefix = "=" * level
+      "\n\n#{prefix} #{text} #{prefix}\n"
+    end
+
+    def paragraph(text)
+      "\n#{text}\n"
+    end
+
+    def block_code(code, language)
+      "\n{{{#!#{language}\n#{code}}}}\n"
+    end
+
+    def block_quote(quote)
+      quote.each_line.map {|l| "    #{l}"}.join
+    end
+
+    def list(contents, list_type)
+      contents
+    end
+
+    def list_item(text, list_type)
+      "\n * #{text}"
+    end
+
+    def link(link, title, content)
+      "[[#{link}|#{content}]]"
+    end
+
+    def double_emphasis(text)
+      "'''#{text}'''"
+    end
+
+    def emphasis(text)
+      "''#{text}''"
+    end
+  end
 end
